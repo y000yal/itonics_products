eecfbe08233537662aa1f4bcf0348eae08ec7292
yoel limbu <yoyal.limbu@gmail.com>
2023-12-09 19:36:39 +0545

    Adde few modules

---
+++ sites/all/modules/yoyal_itonics_products/js/custom.js
@@ -0,0 +1,12 @@
+(function ($) {
+  Drupal.behaviors.mymoduleChosenInit = {
+    attach: function (context, settings) {
+      $('.chosen-select', context).chosen({
+        // Add Chosen options as needed.
+        // For example:
+        width: '100%',
+        // Any other Chosen options...
+      });
+    }
+  };
+})(jQuery);
--- sites/all/modules/yoyal_itonics_products/yoyal_itonics_products_helper.php
+++ sites/all/modules/yoyal_itonics_products/yoyal_itonics_products_helper.php
@@ -24,4 +24,32 @@ class YoyalItonicsProductsHelper {
 
     return $products;
   }
+  public static function  yoyal_itonics_products_get_all_categories (){
+    $categories = array();
+
+    // Fetch products from the 'yoyal_itonics_products' table.
+    $query = db_select('yoyal_itonics_categories', 'c')
+      ->fields('c')
+      ->execute();
+
+    // Loop through the query results to build an array of products.
+    foreach ($query as $result) {
+      $categories[] = array(
+        'cid' => $result->cid,
+        'title' => $result->title,
+        'description' => $result->description
+      );
+    }
+
+    return $categories;
+  }
+  public static function get_category_list (){
+    $all = self::yoyal_itonics_products_get_all_categories();
+
+    $categories = [];
+    foreach ($all as $key => $a) {
+      $categories[$a['cid']] = $a['title'];
+    }
+    return $categories;
+  }
 }
